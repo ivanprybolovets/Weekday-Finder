a364959f2320454d950e7b403114624a61f49e21
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-05 11:43:37 +0300

    Initial Commit

---
+++ Weekday Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weekday Finder
+//
+//  Created by Ivan Prybolovetz on 4/5/20.
+//  Copyright © 2020 Ivan Prybolovetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
