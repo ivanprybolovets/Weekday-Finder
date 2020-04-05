727bcde64ec43e4853ba66d962f557de5898f9c4
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-05 13:08:29 +0300

    Add logics a with code  for findDayButton  ,Number keyboard ,Touches Began(remove the keyboard by touch)

--- Weekday Finder/ViewController.swift
+++ Weekday Finder/ViewController.swift
@@ -24,5 +24,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func findDayButton(_ sender: UIButton) {
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTF.text!)
+        dateComponents.month = Int(monthTF.text!)
+        dateComponents.year = Int(yearTF.text!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let date = calendar.date(from: dateComponents)
+        
+        let weekDay = dateFormatter.string(from: date! )
+        
+        resultLabel.text = weekDay
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true )
     }
 }
